a879913d089970fc7c5f319374a2dc7e28fcfa70
wseem85 <engwseem2@gmail.com>
2025-09-17 10:42:37 +0300

    fixing protecting the get reviews on an artwork route

--- backend/routes/reviewRouter.js
+++ backend/routes/reviewRouter.js
@@ -10,7 +10,6 @@ const {
 const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
-router.use(protect);
 
 router
   .route('/')
