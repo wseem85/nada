1b5ec95f644318c218473a053644ab4c711f51a9
wseem85 <engwseem2@gmail.com>
2025-09-17 09:53:13 +0300

    adding yaml file to frontend and admin to fix deployed version to work with react router dom

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { port: 5174 },
+
+  build: {
+    outDir: 'dist',
+  },
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: { port: 5173 },
+
+  build: {
+    outDir: 'dist',
+  },
 });
